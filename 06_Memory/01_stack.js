e99472f72912a50d0919d465d08d5cc07dea6b84
Manish Choudhary <me.developer4u@gmail.com>
2025-03-22 14:32:15 +0530

    Add files via upload

---
+++ 06_Memory/01_stack.js
@@ -0,0 +1,8 @@
+// values copy to another variable
+let myyoutubename ="hiteshchoudhray.com"
+
+let anothername = myyoutubename
+anothername="chaiaurcode"
+
+console.log(myyoutubename);
+console.log(anothername);
---
+++ 06_Memory/02_heap_memory.js
@@ -0,0 +1,12 @@
+// change in original value 
+let useone={
+    email:"user@google.com",
+    upi:"user@ybl"
+ }
+
+ let usertwo=useone
+
+ usertwo.email="manish@google.com"
+ 
+ console.log(useone.email)    // both the values are same in the outputṇ
+ console.log(usertwo.email)
