e2548a7ef708f6841340cc56ae3a92d67af4814b
Manish Choudhary <me.developer4u@gmail.com>
2025-03-22 14:30:09 +0530

    Add files via upload

---
+++ 01_Variables/02_var_let_const.js
@@ -0,0 +1,22 @@
+const accountId = 112233
+let accountEmail ="me.manish@gmail.com"
+var accouuntPassword ="112233"
+accountCity = "jaipur" // not use to this type of declarations in your programs
+let accountState;
+
+// accountId = "2"  //not allowedN 
+
+accountEmail = "hc@gmail"
+accountPassword ="938898hhi40"
+accountCity ="bengaluru"
+
+console.log(accountId);
+
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
+
+
