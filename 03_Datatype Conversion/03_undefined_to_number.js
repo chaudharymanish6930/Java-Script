4c8b1175c077efc4b389e0b2f5c3502f7e940354
Manish Choudhary <me.developer4u@gmail.com>
2025-03-24 20:55:23 +0530

    Update 03_undefined_to_number.js

--- 03_Datatype Conversion/03_undefined_to_number.js
+++ 03_Datatype Conversion/03_undefined_to_number.js
@@ -1,7 +1,7 @@
 let score3 =undefined
 
-console.log(typeof(score3));
+console.log(typeof(score3));  // undefined
 
-let ValueInNumber = Number(score3)
-console.log(typeof ValueInNumber);
-console.log(ValueInNumber);
+let ValueInNumber = Number(score3) 
+console.log(typeof ValueInNumber);  // numbber
+console.log(ValueInNumber);   // NaN
