802ade6d58121214437c600e5100cefbe8723368
Manish Choudhary <me.developer4u@gmail.com>
2025-03-24 20:50:03 +0530

    Update 01_string_to_number.js

--- 03_Datatype Conversion/01_string_to_number.js
+++ 03_Datatype Conversion/01_string_to_number.js
@@ -2,25 +2,20 @@
 let score2 ="33"
 console.log(typeof(score2));
 
-let valueInNumber = Number(score2)
-console.log(typeof (valueInNumber));
-console.log(valueInNumber);
+let valueInNumber = Number(score2)     // string
+console.log(typeof (valueInNumber));   // number
+console.log(valueInNumber);   // 33
 
  //2
 let score ="33sfsg"
-console.log(typeof score);
+console.log(typeof score);    // string
 
-let valueinNumber = Number(score)
-console.log(typeof valueinNumber);
-console.log(valueinNumber);
-
-
-
-
-
+let valueinNumber = Number(score)   
+console.log(typeof valueinNumber);  // number
+console.log(valueinNumber);  /// NaN
 
  /// num into string
-let o=33
+let o=33  
 let r=String(o)
-console.log(r);
-console.log(typeof r);
+console.log(r);   // 33
+console.log(typeof r); // string
