5ad10e1aae71fc964f246740afd153424a9b0d58
Manish Choudhary <me.developer4u@gmail.com>
2025-03-30 08:22:55 +0530

    Update 02_date_given_by_user.js

--- 10_Date and Time/02_date_given_by_user.js
+++ 10_Date and Time/02_date_given_by_user.js
@@ -1,4 +1,4 @@
-let mycreate = new Date(2024, 4,10);
+let mycreate = new Date(2024, 4,10); // months start  with 0
 console.log(mycreate);
 console.log(mycreate.toString());
 console.log(mycreate.toDateString());
