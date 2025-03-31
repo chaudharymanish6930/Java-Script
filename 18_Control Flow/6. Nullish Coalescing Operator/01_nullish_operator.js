65c379e58682b87cc2c49e86c18e9feb20ca1429
Manish Choudhary <me.developer4u@gmail.com>
2025-03-31 08:34:57 +0530

    Update 01_nullish_operator.js

--- 18_Control Flow/6. Nullish Coalescing Operator/01_nullish_operator.js
+++ 18_Control Flow/6. Nullish Coalescing Operator/01_nullish_operator.js
@@ -5,7 +5,7 @@ let val2 = null ?? 10
 console.log(val2)  // 10
 
 let val3= undefined ?? 20
-console.log(val3)
+console.log(val3)   // 20
 
 let val4 = null ?? 10 ?? 20
-console.log(val4)
+console.log(val4)   // 10
