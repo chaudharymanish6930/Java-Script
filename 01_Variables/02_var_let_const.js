f6cf03ae4b2f9aab58808ef7b044a03100231bcb
Manish Choudhary <me.developer4u@gmail.com>
2026-02-25 14:25:19 +0530

    Update

--- 01_Variables/02_var_let_const.js
+++ 01_Variables/02_var_let_const.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-const accountId = 112233
 let accountEmail ="me.manish@gmail.com"
 var accouuntPassword ="112233"
 accountCity = "jaipur" // not use to this type of declarations in your programs
@@ -11,37 +9,11 @@ accountEmail = "hc@gmail"
 accountPassword ="938898hhi40"
 accountCity ="bengaluru"
 
-console.log(accountId);
+console.log(accountEmail);
 
 /* 
 prefer not to use var
 because of issue in block scope and functional scope
 */
 
-console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
-
-
-=======
-const accountId = 112233
-let accountEmail ="me.manish@gmail.com"
-var accouuntPassword ="112233"
-accountCity = "jaipur" // not use to this type of declarations in your programs
-let accountState;
-
-// accountId = "2"  //not allowedN 
-
-accountEmail = "hc@gmail"
-accountPassword ="938898hhi40"
-accountCity ="bengaluru"
-
-console.log(accountId);
-
-/* 
-prefer not to use var
-because of issue in block scope and functional scope
-*/
-
-console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
-
-
->>>>>>> 5c3f3ff (first)
+console.table([accountEmail, accountPassword, accountCity,accountState])
