fa0d524bdb05fec12d8cc0c9b97c7d6dbe380ffa
Manish Choudhary <me.developer4u@gmail.com>
2026-03-16 20:54:43 +0530

    Update

---
+++ 22_DOM/02_ApplyOnWikiPedia.js
@@ -0,0 +1,117 @@
+document.querySelectorAll("h1");
+NodeList [h1#firstHeading.firstHeading.mw-first-heading]0: h1#firstHeading.firstHeading.mw-first-headinglength: 1[[Prototype]]: NodeList
+MediaWikiMetricsClientLogger.js:19 The stream mediawiki.client.session_tick is out of sample. No event will be sent
+MediaWikiMetricsClientLogger.logWarning @ MediaWikiMetricsClientLogger.js:19
+MetricsClient.processSubmitCall @ MetricsClient.js:345
+MetricsClient.submit @ MetricsClient.js:297
+sessionTick @ sessionTick.js:96
+run @ sessionTick.js:134
+setTimeout
+run @ sessionTick.js:137
+setActive @ sessionTick.js:150
+onVisibilitychange @ sessionTick.js:174Understand this warning
+const sel = document.querySelectorAll("h2");
+undefined
+sel
+NodeList(15) [h2.vector-pinnable-header-label, h2#History, h2#Trademark, h2#Website_client-side_usage, h2#Other_usage, h2#Execution, h2#Features, h2#Syntax, h2#Security, h2#Development_tools, h2#Related_technologies, h2#Notes, h2#References, h2#Further_reading, h2#External_links]0: h2.vector-pinnable-header-label1: h2#History2: h2#Trademark3: h2#Website_client-side_usage4: h2#Other_usage5: h2#Execution6: h2#Features7: h2#Syntax8: h2#Security9: h2#Development_tools10: h2#Related_technologies11: h2#Notes12: h2#References13: h2#Further_reading14: h2#External_linkslength: 15[[Prototype]]: NodeList
+sel[0].innerText;
+''
+sel[0].innerText();
+VM504:1 Uncaught TypeError: sel[0].innerText is not a function
+    at <anonymous>:1:8
+(anonymous) @ VM504:1Understand this error
+MediaWikiMetricsClientLogger.js:19 The stream mediawiki.client.session_tick is out of sample. No event will be sent
+MediaWikiMetricsClientLogger.logWarning @ MediaWikiMetricsClientLogger.js:19
+MetricsClient.processSubmitCall @ MetricsClient.js:345
+MetricsClient.submit @ MetricsClient.js:297
+sessionTick @ sessionTick.js:96
+run @ sessionTick.js:134
+setActive @ sessionTick.js:150
+onVisibilitychange @ sessionTick.js:174Understand this warning
+MediaWikiMetricsClientLogger.js:19 The stream mediawiki.client.session_tick is out of sample. No event will be sent
+MediaWikiMetricsClientLogger.logWarning @ MediaWikiMetricsClientLogger.js:19
+MetricsClient.processSubmitCall @ MetricsClient.js:345
+MetricsClient.submit @ MetricsClient.js:297
+sessionTick @ sessionTick.js:96
+run @ sessionTick.js:134
+setTimeout
+run @ sessionTick.js:137
+setActive @ sessionTick.js:150
+onVisibilitychange @ sessionTick.js:174Understand this warning
+sel[2].innerText;
+'Trademark'
+sel[0].innerHTML;
+'Contents'
+MediaWikiMetricsClientLogger.js:19 The stream mediawiki.client.session_tick is out of sample. No event will be sent
+MediaWikiMetricsClientLogger.logWarning @ MediaWikiMetricsClientLogger.js:19
+MetricsClient.processSubmitCall @ MetricsClient.js:345
+MetricsClient.submit @ MetricsClient.js:297
+sessionTick @ sessionTick.js:96
+run @ sessionTick.js:134
+setActive @ sessionTick.js:150
+requestIdleCallback
+setActiveDebounce @ sessionTick.js:166Understand this warning
+MediaWikiMetricsClientLogger.js:19 The stream mediawiki.client.session_tick is out of sample. No event will be sent
+MediaWikiMetricsClientLogger.logWarning @ MediaWikiMetricsClientLogger.js:19
+MetricsClient.processSubmitCall @ MetricsClient.js:345
+MetricsClient.submit @ MetricsClient.js:297
+sessionTick @ sessionTick.js:96
+run @ sessionTick.js:134
+setTimeout
+run @ sessionTick.js:137
+setActive @ sessionTick.js:150
+requestIdleCallback
+setActiveDebounce @ sessionTick.js:166Understand this warning
+sel.forEach(function(h) {
+    h.style.color='green';});
+undefined
+2MediaWikiMetricsClientLogger.js:19 The stream mediawiki.client.session_tick is out of sample. No event will be sent
+MediaWikiMetricsClientLogger.logWarning @ MediaWikiMetricsClientLogger.js:19
+MetricsClient.processSubmitCall @ MetricsClient.js:345
+MetricsClient.submit @ MetricsClient.js:297
+sessionTick @ sessionTick.js:96
+run @ sessionTick.js:134
+setActive @ sessionTick.js:150
+requestIdleCallback
+setActiveDebounce @ sessionTick.js:166Understand this warning
+MediaWikiMetricsClientLogger.js:19 The stream mediawiki.client.session_tick is out of sample. No event will be sent
+MediaWikiMetricsClientLogger.logWarning @ MediaWikiMetricsClientLogger.js:19
+MetricsClient.processSubmitCall @ MetricsClient.js:345
+MetricsClient.submit @ MetricsClient.js:297
+sessionTick @ sessionTick.js:96
+run @ sessionTick.js:134
+setTimeout
+run @ sessionTick.js:137
+setActive @ sessionTick.js:150
+requestIdleCallback
+setActiveDebounce @ sessionTick.js:166Understand this warning
+sel.forEach(function(h) {
+    h.style.color='green';
+    h.style.backgroundColor='blue';
+});
+
+undefined
+MediaWikiMetricsClientLogger.js:19 The stream mediawiki.client.session_tick is out of sample. No event will be sent
+MediaWikiMetricsClientLogger.logWarning @ MediaWikiMetricsClientLogger.js:19
+MetricsClient.processSubmitCall @ MetricsClient.js:345
+MetricsClient.submit @ MetricsClient.js:297
+sessionTick @ sessionTick.js:96
+run @ sessionTick.js:134
+setTimeout
+run @ sessionTick.js:137
+setTimeout
+run @ sessionTick.js:137
+setActive @ sessionTick.js:150
+requestIdleCallback
+setActiveDebounce @ sessionTick.js:166Understand this warning
+sel.forEach(function(h) {
+    h.style.color='green';
+    h.style.backgroundColor='blue';
+});
+undefined
+sel.forEach(function(h) {
+    h.style.color='green';
+    h.style.backgroundColor='blue';
+    h.style.padding='30px';
+});
+undefined
