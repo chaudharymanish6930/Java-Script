cc1e2a6fc3b50acf99c2b09f4d15ac7831fdca7c
Manish Choudhary <me.developer4u@gmail.com>
2026-04-03 14:22:36 +0530

    DOM related Project

--- 23_DOM Projects/1-ColorChanger/chaiaurcode.js
+++ 23_DOM Projects/1-ColorChanger/chaiaurcode.js
@@ -1,4 +1,4 @@
-const buttons=document.querySelector('.button');
+const buttons=document.querySelectorAll('.button');
 const body=document.querySelector('body');
 
 buttons.forEach(function (button) {
@@ -6,5 +6,75 @@ buttons.forEach(function (button) {
     button.addEventListener("click", function(e){
         console.log(e);
         console.log(e.target);
+        
+        if(e.target.id ==='grey'){
+        // body.style.bgc=grey;
+        body.style.backgroundColor=e.target.id;
+        }
+        else if(e.target.id ==='yellow'){
+        body.style.backgroundColor=e.target.id;
+        }
+        else if(e.target.id ==='blue'){
+        body.style.backgroundColor=e.target.id;
+        }
+        else if(e.target.id ==='white'){
+        body.style.backgroundColor=e.target.id;
+        }
+        else if(e.target.id ==='purple'){
+        body.style.backgroundColor=e.target.id;
+        }
+
     })
+});
+
+
+buttons.forEach(function (button) {
+    console.log(button);
+    button.addEventListener("mouseover", function(e){
+        console.log(e);
+        console.log(e.target);
+        
+        if(e.target.id ==='grey'){
+        // body.style.bgc=grey;
+        body.style.backgroundColor=e.target.id;
+        }
+        else if(e.target.id ==='yellow'){
+        body.style.backgroundColor=e.target.id;
+        }
+        else if(e.target.id ==='blue'){
+        body.style.backgroundColor=e.target.id;
+        }
+        else if(e.target.id ==='white'){
+        body.style.backgroundColor=e.target.id;
+        }
+        else if(e.target.id ==='purple'){
+        body.style.backgroundColor=e.target.id;
+        }
+
+    })
+});
+
+
+// switch case
+buttons.forEach(function (button) {
+    button.addEventListener("click", function(e){
+
+        switch(e.target.id) {
+            case "red":
+                console.log("Red button clicked");
+                break;
+
+            case "green":
+                console.log("Green button clicked");
+                break;
+
+            case "blue":
+                console.log("Blue button clicked");
+                break;
+
+            default:
+                console.log("Unknown button");
+        }
+
+    });
 });
