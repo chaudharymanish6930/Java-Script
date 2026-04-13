149b3e568e484e49643d38e90b628fa0353838ae
Manish Choudhary <me.developer4u@gmail.com>
2026-04-13 23:42:47 +0530

    Function is also an Object

---
+++ 31_ObjectOrientedProgramming/05_Function.js
@@ -0,0 +1,10 @@
+function MultiplyNUM(num){
+    return num*5;
+}
+
+const my=MultiplyNUM(5);
+console.log(my)
+
+MultiplyNUM.power=30
+console.log(MultiplyNUM.power)
+console.log(MultiplyNUM.prototype)
